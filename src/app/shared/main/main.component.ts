230b5e7525ba6efd1b899351faa2207624f97b45
Federico Perez <fperez@8amarketing.com>
2018-08-23 17:57:31 -0300

    Updated styles navbar, add some valid range for input

--- src/app/shared/main/main.component.ts
+++ src/app/shared/main/main.component.ts
@@ -10,6 +10,7 @@ export class MainComponent implements OnInit {
   tasks: Array<any> = localStorage.getItem( 'items' ) ? JSON.parse( localStorage.getItem( 'items' ) ) : [];
   validated: any = false;
   example: any = false;
+  alertMsj: string = '';
   
   constructor() {}
   
@@ -22,10 +23,17 @@ export class MainComponent implements OnInit {
 
   addNew( value: string ) {
     if ( value != '' ) {
-      this.tasks.push( value );
-      localStorage.setItem( 'items', JSON.stringify( this.tasks ) );
+      if (value.length >= 2 && value.length <= 15) { 
+        this.tasks.push( value );
+        localStorage.setItem( 'items', JSON.stringify( this.tasks ) );
+      }else{
+        this.validated = true;
+        this.alertMsj = 'El item debe tener entre 2 y 15 caracteres!';
+        setTimeout(function(){ this.validated = false; }.bind( this ), 2000 );
+      }
     } else {
       this.validated = true;
+      this.alertMsj = 'Por favor, agregar item valido!';
       setTimeout(function(){ this.validated = false; }.bind( this ), 2000 );
     }
   }
